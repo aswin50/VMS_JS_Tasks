dbab6610be88f691a733128428d7a6e7770b1c8a
Ragupathi <Ragupathi.Venkadachalam@cognizant.com>
2023-01-20 10:50:59 +0530

    msg

---
+++ Module 3/index.js
@@ -0,0 +1,6 @@
+const body=document.body
+const div=document.querySelector('div');
+const sp1=document.querySelector('#sp1');
+const sp2=document.querySelector('#sp2');
+console.log(sp1.innertext)
+console.log(sp2.textcontent)
